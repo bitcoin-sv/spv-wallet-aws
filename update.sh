1016767728d4ed04a724fe5b08cef4eed4d6d5f2
Damian Orzepowski <damian.orzepowski@4chain.studio>
2024-03-05 16:41:24 +0100

    feat: update.sh move assets based on configuration in yaml

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Function to update test environment
-updateDev() {
-  echo "Updating test environment to version $1"
+update() {
+  set -e
+  echo "Updating $environment environment to version $version"
+
+  echo "Loading config from file ./config/$environment.yml"
+  eval "$(yq '.' "./config/$environment.yml" -o shell)"
+  file_asset_prefix=${file_asset_prefix%?}
 
   # Generate a template for the target environment
-  cdk synth --context=deployment=test
+  # I want to fail the script if this command fail
+  cdk synth --context=deployment="$environment"
 
   # Update path in assets configuration for temporary path
   sed -i.bak 's/latest/new/g' cdk.out/EksStack.assets.json
@@ -13,89 +19,19 @@ updateDev() {
   # Publish assets on AWS S3
   npx cdk-assets publish -p cdk.out/EksStack.assets.json -v
 
-  # Backup latest template and assets
-  aws s3 --recursive mv s3://spv-wallet-test-template/spv-wallet/latest s3://spv-wallet-test-template/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/latest s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/latest s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/old
-  # Move new template and assets to latest
-  aws s3 --recursive mv s3://spv-wallet-test-template/spv-wallet/new s3://spv-wallet-test-template/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/new s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/new s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/latest
-}
 
-# Function to update test environment
-updateTest() {
-  echo "Updating test environment to version $1"
+  echo "Moving template to backup folder"
+  aws s3 --recursive mv "s3://$template_bucket_name/$file_asset_prefix" "s3://$template_bucket_name/spv-wallet/old"
+  echo "Updating template"
+  aws s3 --recursive mv "s3://$template_bucket_name/spv-wallet/new" "s3://$template_bucket_name/$file_asset_prefix"
+  for regionVar in ${!file_asset_region_set_*}; do
+      region=${!regionVar}
+      echo "Moving assets in region $region to backup folder"
+      aws s3 --recursive mv "s3://$file_asset_bucket_name_prefix-$region/$file_asset_prefix" "s3://$file_asset_bucket_name_prefix-$region/spv-wallet/old"
+      echo "Updating assets in region $region"
+      aws s3 --recursive mv "s3://$file_asset_bucket_name_prefix-$region/spv-wallet/new" "s3://$file_asset_bucket_name_prefix-$region/$file_asset_prefix"
+  done
 
-  # Generate a template for the target environment
-  cdk synth --context=deployment=test
-
-  # Update path in assets configuration for temporary path
-  sed -i.bak 's/latest/new/g' cdk.out/EksStack.assets.json
-
-  # Publish assets on AWS S3
-  npx cdk-assets publish -p cdk.out/EksStack.assets.json -v
-
-  # Backup latest template and assets
-  aws s3 --recursive mv s3://spv-wallet-test-template/spv-wallet/latest s3://spv-wallet-test-template/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/latest s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/latest s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/old
-  # Move new template and assets to latest
-  aws s3 --recursive mv s3://spv-wallet-test-template/spv-wallet/new s3://spv-wallet-test-template/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/new s3://spv-wallet-test-marketplace-assets-us-east-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/new s3://spv-wallet-test-marketplace-assets-eu-central-1/spv-wallet/latest
-}
-
-# Function to update prod environment
-updateProd() {
-  # Generate a template for the target environment
-  cdk synth --context=deployment=prod
-
-  # Update path in assets configuration for temporary path
-  sed -i 's/latest/new/g' cdk.out/EksStack.assets.json
-
-  # Upload assets to the temporary path (npx is going to use the default AWS profile or AWS_PROFILE if set)
-  npx cdk-assets publish -p cdk.out/EksStack.assets.json -v
-
-  # Backup latest templates and assets
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-eu-west-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-2/spv-wallet/latest s3://spv-wallet-marketplace-assets-eu-west-2/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-3/spv-wallet/latest s3://spv-wallet-marketplace-assets-eu-west-3/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-west-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-us-west-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-west-2/spv-wallet/latest s3://spv-wallet-marketplace-assets-us-west-2/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-sa-east-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-sa-east-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-north-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-eu-north-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ca-central-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-ca-central-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-south-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-south-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-northeast-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-2/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-northeast-2/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-3/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-northeast-3/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-southeast-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-southeast-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-southeast-2/spv-wallet/latest s3://spv-wallet-marketplace-assets-ap-southeast-2/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-central-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-eu-central-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-east-1/spv-wallet/latest s3://spv-wallet-marketplace-assets-us-east-1/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-east-2/spv-wallet/latest s3://spv-wallet-marketplace-assets-us-east-2/spv-wallet/old
-  aws s3 --recursive mv s3://spv-wallet-template/spv-wallet/latest s3://spv-wallet-template/spv-wallet/old
-
-  # Move new template and assets to latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-1/spv-wallet/new s3://spv-wallet-marketplace-assets-eu-west-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-2/spv-wallet/new s3://spv-wallet-marketplace-assets-eu-west-2/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-west-3/spv-wallet/new s3://spv-wallet-marketplace-assets-eu-west-3/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-west-1/spv-wallet/new s3://spv-wallet-marketplace-assets-us-west-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-west-2/spv-wallet/new s3://spv-wallet-marketplace-assets-us-west-2/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-sa-east-1/spv-wallet/new s3://spv-wallet-marketplace-assets-sa-east-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-north-1/spv-wallet/new s3://spv-wallet-marketplace-assets-eu-north-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ca-central-1/spv-wallet/new s3://spv-wallet-marketplace-assets-ca-central-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-south-1/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-south-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-1/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-northeast-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-2/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-northeast-2/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-northeast-3/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-northeast-3/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-southeast-1/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-southeast-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-ap-southeast-2/spv-wallet/new s3://spv-wallet-marketplace-assets-ap-southeast-2/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-eu-central-1/spv-wallet/new s3://spv-wallet-marketplace-assets-eu-central-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-east-1/spv-wallet/new s3://spv-wallet-marketplace-assets-us-east-1/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-marketplace-assets-us-east-2/spv-wallet/new s3://spv-wallet-marketplace-assets-us-east-2/spv-wallet/latest
-  aws s3 --recursive mv s3://spv-wallet-template/spv-wallet/new s3://spv-wallet-template/spv-wallet/latest
 }
 
 # Default values
@@ -147,9 +83,7 @@ yq ".helm_chart_version=\"$version\"" -i "./config/${environment}.yml"
 
 # Call functions based on environment value
 case $environment in
-    dev) updateDev "$version" ;;
-    test) updateTest "$version" ;;
-    prod) updateProd "$version" ;;
+    dev|test|prod) update ;;
     *) echo "Not implemented support for environment $environment"; exit 1 ;;
 esac
 
